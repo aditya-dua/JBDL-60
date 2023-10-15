c889b54bbd68df9021b0e2ba6a3329421277d739
aditya-dua <adityaduatechm@gmail.com>
2023-10-15 22:14:52 +0530

    API
    API

--- L6_Intro_to_SB/src/main/java/com/example/demo/L6IntroToSbApplication.java
+++ L6_Intro_to_SB/src/main/java/com/example/demo/L6IntroToSbApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,6 +19,23 @@ public class L6IntroToSbApplication {
 	public String test() {
 		System.out.println("API request recieved");
 		return "Test API Works!";
+	}
+	
+	@RequestMapping("/greet")
+	public String greeting(@RequestParam String name) {
+		String msg = "";
+		if(name ==null ||name.equals("") ) {
+			msg = "Hello World!";
+			
+		}else {
+			msg = "Hello "+name;
+		}
+		return msg;
+	}
+	
+	@RequestMapping("/greet/{name}")
+	public String testFunc(@PathVariable String name) {
+		return "Hello From testFunc with "+name;
 	}
 
 }
