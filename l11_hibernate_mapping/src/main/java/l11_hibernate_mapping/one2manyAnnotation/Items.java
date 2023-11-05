95fefac101aaee6cee7927e68df26e4327977c29
aditya-dua <adityaduatechm@gmail.com>
2023-11-05 20:18:36 +0530

    One to Many Mapping
    One to Many Mapping

---
+++ l11_hibernate_mapping/src/main/java/l11_hibernate_mapping/one2manyAnnotation/Items.java
@@ -0,0 +1,80 @@
+package l11_hibernate_mapping.one2manyAnnotation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="items-o2m-annotation")
+public class Items {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue
+	private int id;
+	
+	@Column(name="itemId")
+	private String itemId;
+	
+	@Column(name="total")
+	private int total;
+	
+	@Column(name="quantity")
+	private int quantity;
+	
+	@ManyToOne
+	@JoinColumn(name="cart_id",nullable = false)
+	private Cart cart;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Cart getCart() {
+		return cart;
+	}
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	@Override
+	public String toString() {
+		return "Items [id=" + id + ", itemId=" + itemId + ", total=" + total + ", quantity=" + quantity + ", cart="
+				+ cart + "]";
+	}
+	public Items() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Items(String itemId, int total, int quantity, Cart cart) {
+		super();
+		this.itemId = itemId;
+		this.total = total;
+		this.quantity = quantity;
+		this.cart = cart;
+	}
+	
+
+}
