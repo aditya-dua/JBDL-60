7f02fa4d8cde1a49da5e09645737aa15d69dcd30
aditya-dua <adityaduatechm@gmail.com>
2023-12-03 21:39:58 +0530

    Kafka
    Kafka

---
+++ L16_Springboot_Kafka/src/main/java/com/example/demo/KafkaSender.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Linux/Mac: ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic greet_wrols --from-beginning
+ * Windows: .\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic hello_world --from-beginning
+
+ */
+
+@Service
+public class KafkaSender {
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	String kafkaTopic = "hello_world";
+	
+	public void send(String message) {
+		kafkaTemplate.send(kafkaTopic,message);
+	}
+
+}
---
+++ L16_Springboot_Kafka/src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class WebController {
+
+	@Autowired
+	KafkaSender kafkaSender;
+	
+	
+	@GetMapping(value="/kafka")
+	public String producer(@RequestParam String message) {
+		kafkaSender.send(message);
+		return "Message send to kafka topic: "+kafkaSender.kafkaTopic+" message:"+message;
+	}
+}
